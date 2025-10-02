59af175b1952566a54a4396596747d2a8b13e9a7
sulaimondawood <sulaimondawod@gmail.com>
2025-10-02 11:55:09 +0100

    Project bootstrapping

---
+++ src/main/java/com/dawood/business_discovery/BusinessDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.dawood.business_discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessDiscoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dawood/business_discovery/BusinessDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dawood.business_discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
