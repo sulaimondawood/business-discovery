5abd8d1c79aa92f4d9c2d596a024252242014e42
sulaimondawood <sulaimondawod@gmail.com>
2025-10-02 16:17:09 +0100

    added user entity

---
+++ src/main/java/com/dawood/business_discovery/entities/User.java
@@ -0,0 +1,43 @@
+package com.dawood.business_discovery.entities;
+
+import com.dawood.business_discovery.enums.UserRole;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class User {
+
+  @Id
+  @GeneratedValue
+  @Column(unique = true)
+  private Long id;
+
+  @Column(nullable = false)
+  private String fullname;
+
+  @Column(unique = true, nullable = false)
+  private String email;
+
+  @Column(nullable = false)
+  private String password;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  private UserRole role;
+
+}
---
+++ src/main/java/com/dawood/business_discovery/enums/UserRole.java
@@ -0,0 +1,6 @@
+package com.dawood.business_discovery.enums;
+
+public enum UserRole {
+  USER,
+  ADMIN,
+}
